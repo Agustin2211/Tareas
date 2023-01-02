74717e4609b42741391144ddb7e0080b98734842
Agustin2211 <72230884+Agustin2211@users.noreply.github.com>
2023-01-02 15:14:58 -0300

    Terminado

--- src/app/components/tareas/tareas.component.ts
+++ src/app/components/tareas/tareas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Tarea } from 'src/app/models/tarea';
 
 @Component({
   selector: 'app-tareas',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./tareas.component.css']
 })
 export class TareasComponent {
+
+  listaTareas: Tarea[] = []
+  nombreTarea = '';
+
+  agregarTarea(){
+
+    //Creacion del Objeto Tarea
+    const tarea: Tarea={
+      nombre: this.nombreTarea,
+      estado: false
+    }
+
+    //Agregar el objeto Tarea al array
+    this.listaTareas.push(tarea);
+
+    //Resetear input
+    this.nombreTarea = '';
+  }
+
+  eliminarTarea(index: number): void{
+    this.listaTareas.splice(index, 1);
+  }
+
+  actualizarTarea(tarea: Tarea, index: number): void{
+    this.listaTareas[index].estado = !tarea.estado
+  }
 
 }
